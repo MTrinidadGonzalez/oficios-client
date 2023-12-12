4043ddc2303deab6ce2a90c863f176d5c637ed68
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-12-12 16:27:53 -0300

    cambios css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,23 +8,50 @@ import {NewMessageContext} from '../../context/NewMessageContext'
 
 const NavBar = () => {
     const { newMessageList } = useContext(NewMessageContext);
-  
+    const [showMobileItems, setShowMobileItems] = useState(false);
+
+  const toggleMobileItems = () => {
+    setShowMobileItems(!showMobileItems);
+  };
 
     return (
+  <>
+   <div className="nav-mobile">
+        <div className="responsive-nav-hamburger" onClick={toggleMobileItems}>
+          <ion-icon name="reorder-three-outline"></ion-icon>
+        </div>
+        {showMobileItems && (
+          <div className="items-nav-mobile-container">
+            <Link to="/home" className="item-nav">HOME</Link>
+            <Link to="/users" className="item-nav">USUARIOS</Link>
+            <Link to="/chatsList" className="item-nav">
+              <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+              {newMessageList.length > 0 ? (
+                <span className="new-messages-cuantity">{newMessageList.length}</span>
+              ) : null}
+            </Link>
+            <Link to="/profile" className="item-nav">MI PERFIL</Link>
+            <CarrarSesion />
+          </div>
+        )}
+      </div>
+
+     
       <div className="navBar" >
       <Link to="/home" className="item-nav" >HOME</Link>
-      
-             <Link to="/users" className="item-nav" >USUARIOS</Link>
-             <Link to="/chatsList" className="item-nav" >
-             <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
-               {newMessageList.length > 0 ? (
-                 <span className="new-messages-cuantity">{newMessageList.length}</span>
-               ) : null}
-             </Link>
-             <Link to="/profile" className="item-nav" >MI PERFIL</Link>
-             <CarrarSesion  />
+      <Link to="/users" className="item-nav" >USUARIOS</Link>
+      <Link to="/chatsList" className="item-nav" >
+          <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+            {newMessageList.length > 0 ? (
+            <span className="new-messages-cuantity">{newMessageList.length}</span>
+            ) : null}
+      </Link>
+      <Link to="/profile" className="item-nav" >MI PERFIL</Link>
+      <CarrarSesion />
       </div>
+      </>
     );
   };
   
   export default NavBar;
+
