56ec425c29a08d594a12cf0e403ffc51b4aa2633
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-12-11 21:01:33 -0300

    .

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -3,9 +3,9 @@ import  UserCard from '../UserCard/UserCard'
 import { useState, useEffect } from 'react'
 import NavBar from '../NavBar/NavBar'
 
+
 const UserProfile = () => {
     const [user, setUser] = useState({})
-    const [apiImgProfile, setApiImgProfile] = useState(null);
 
     useEffect(() => {
         const getUser = async () => {
@@ -25,7 +25,6 @@ const UserProfile = () => {
                         zona: result.zona
                     }
                     setUser({ ...userData })
-                    const imgResponse = await userService.getImgProfiles(result.imgProfile);
                 }
                 if (response.data.status === 'error') {
                     if (response.data.error === "No hay token de usuario aún") {
@@ -37,13 +36,6 @@ const UserProfile = () => {
     }, [])
 
     return ( <>
-          {apiImgProfile && (
-                <div>
-                    <p>Imagen del perfil:</p>
-                    <img src={`data:image/jpeg;base64,${apiImgProfile}`} alt="Imagen de perfil" />
-                </div>
-            )}
-
     <NavBar/>
     <div className='containers-generales'>
     <UserCard id={user._id} first_name={user.first_name} 
@@ -53,47 +45,3 @@ const UserProfile = () => {
 }
  
 export default UserProfile;
-
-
-/*
-const UserProfile = () => {
-    const [user, setUser] = useState({})
-
-    useEffect(() => {
-        const getUser = async () => {
-            const userService = new UserService()
-            const response = await userService.getUserProfile()
-                const result = await response.data.payload
-                
-                if (result) {
-                    const userData = {
-                        id: result._id,
-                        alias: result.alias,
-                        first_name: result.first_name,
-                        last_name: result.last_name,
-                        email: result.email,
-                        img: result.imgProfile,
-                        role: result.role,
-                        zona: result.zona
-                    }
-                    setUser({ ...userData })
-                }
-                if (response.data.status === 'error') {
-                    if (response.data.error === "No hay token de usuario aún") {
-                        console.log('response.data.error:', response.data.error)
-                    }
-                }
-        }
-        getUser()
-    }, [])
-
-    return ( <>
-    <NavBar/>
-    <div className='containers-generales'>
-    <UserCard id={user._id} first_name={user.first_name} 
-    last_name={user.last_name} alias={user.alias} email={user.email} zona={user.zona} role={user.role} img={user.img} />
-    </div>
-    </> );
-}
- 
-export default UserProfile;*/
