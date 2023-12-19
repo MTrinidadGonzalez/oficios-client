dd4b14715e106174c7a1fd463921f6f9b862fa26
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-12-19 16:38:06 -0300

    .

--- src/components/PresentationText/PresentationText.jsx
+++ src/components/PresentationText/PresentationText.jsx
@@ -1,21 +1,29 @@
+import React, { useState } from 'react';
+
 const PresentationText = () => {
-   
-   return ( <>
-   <div className="p-presentation-text">
-        <p>
-        ¿Alguna vez has necesitado encontrar un profesional calificado 
-        cerca de ti? ¡Oficios está aquí para hacerlo posible! Esta aplicación te conecta con talentosos individuos que ofrecen una variedad 
-        de servicios de oficio, desde servicios de peluquería hasta  electricista, maestros integradores,plomero, cuidado de personas, y mucho más, dentro de la provincia de Córdoba. 
-        </p>
-     
-        <p>
-        Filtrado por Distancia: ¿Prefieres profesionales cercanos? 
-        Con Oficios, puedes ajustar la distancia de búsqueda y encontrar 
-        expertos en tu área local. Olvídate de largos desplazamientos y 
-        apoya a los talentos locales.
-        </p>
-   </div>
-    </> );
-}
- 
+  const [isPresentationVisible, setIsPresentationVisible] = useState(false);
+
+  const togglePresentation = () => {
+    setIsPresentationVisible(!isPresentationVisible);
+  };
+
+  return (
+    <>
+      <button onClick={togglePresentation} className='btn-about-us'>About Us</button>
+      {isPresentationVisible && (
+        <div className="p-presentation-text">
+          <p>
+            ¿Alguna vez has necesitado encontrar un profesional calificado cerca de ti? ¡Oficios está aquí para hacerlo posible! Esta aplicación te conecta con talentosos individuos que ofrecen una variedad de servicios de oficio, desde servicios de peluquería hasta electricista, maestros integradores, plomero, cuidado de personas, y mucho más, dentro de la provincia de Córdoba.
+          </p>
+
+          <p>
+            Filtrado por Distancia: ¿Prefieres profesionales cercanos? Con Oficios, puedes ajustar la distancia de búsqueda y encontrar expertos en tu área local. Olvídate de largos desplazamientos y apoya a los talentos locales.
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
 export default PresentationText;
+
